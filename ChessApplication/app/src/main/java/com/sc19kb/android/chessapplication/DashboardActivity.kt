4b61d8b2d329c30ca405de79c15b98f1760b118f
Konstantinos <sc19kb@leeds.ac.uk>
2021-11-18 13:59:43 +0000

    Create working Google Login Functionality

---
+++ ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/DashboardActivity.kt
@@ -0,0 +1,31 @@
+package com.sc19kb.android.chessapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+
+        id_txt.text = currentUser?.uid
+        name_txt.text = currentUser?.displayName
+        email_txt.text = currentUser?.email
+
+        sign_out_btn.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/MainActivity.kt
+++ ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.sc19kb.android.chessapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.analytics.FirebaseAnalytics
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private var mFirebaseAnalytics:FirebaseAnalytics?=null
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        /**If user is not authenticated, send him to SignInActivity to authenticate first.
+         * Else send him to DashboardActivity*/
+        Handler().postDelayed({
+            if(user != null){
+                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                startActivity(dashboardIntent)
+                finish()
+            }else{
+                val signInIntent = Intent(this, SignInActivity::class.java)
+                startActivity(signInIntent)
+                finish()
+            }
+        }, 2000)
+
     }
 }
