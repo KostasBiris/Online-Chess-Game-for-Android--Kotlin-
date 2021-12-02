794b4e8b25d1253e2ae5ebcf430dd49a7fbf3887
Konstantinos <sc19kb@leeds.ac.uk>
2021-12-02 12:58:41 +0000

    Create chessbord on console

--- ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/MainActivity.kt
+++ ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/MainActivity.kt
@@ -4,15 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
+private  const val  TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    var chessModel = ChessModel()
 
     //database instance
     private var database= FirebaseDatabase.getInstance()
@@ -24,22 +27,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mAuth = FirebaseAuth.getInstance()
-        val user = mAuth.currentUser
+        Log.d(TAG, "$chessModel")
 
-        /**If user is not authenticated, send him to SignInActivity to authenticate first.
-         * Else send him to DashboardActivity*/
-        Handler().postDelayed({
-            if(user != null){
-                val dashboardIntent = Intent(this, DashboardActivity::class.java)
-                startActivity(dashboardIntent)
-                finish()
-            }else{
-                val signInIntent = Intent(this, SignInActivity::class.java)
-                startActivity(signInIntent)
-                finish()
-            }
-        }, 2000)
+//        mAuth = FirebaseAuth.getInstance()
+//        val user = mAuth.currentUser
+//
+//        /**If user is not authenticated, send him to SignInActivity to authenticate first.
+//         * Else send him to DashboardActivity*/
+//        Handler().postDelayed({
+//            if(user != null){
+//                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+//                startActivity(dashboardIntent)
+//                finish()
+//            }else{
+//                val signInIntent = Intent(this, SignInActivity::class.java)
+//                startActivity(signInIntent)
+//                finish()
+//            }
+//        }, 2000)
 
     }
 }
