9f3a5534bad50a39bd083c731dc40c0b49578c9f
Konstantinos <sc19kb@leeds.ac.uk>
2021-12-06 19:12:04 +0000

    Add Chess Pieces on the Chessboard UI

--- ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/ChessBoard.kt
+++ ChessApplication/app/src/main/java/com/sc19kb/android/chessapplication/ChessBoard.kt
@@ -10,36 +10,94 @@ package com.sc19kb.android.chessapplication
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class ChessBoard(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-    
+
     private val originX = 20f
     private val originY = 200f
     private val cellSize =130f
 
-    @SuppressLint("DrawAllocation")
-    override fun onDraw(canvas: Canvas?) {
+    private final val imgResIDs = setOf(
+        R.drawable.bishop_black,
+        R.drawable.bishop_white,
+        R.drawable.king_black,
+        R.drawable.king_white,
+        R.drawable.queen_black,
+        R.drawable.queen_white,
+        R.drawable.rook_black,
+        R.drawable.rook_white,
+        R.drawable.knight_black,
+        R.drawable.knight_white,
+        R.drawable.pawn_black,
+        R.drawable.pawn_white,
+    )
+    private final val bitmaps = mutableMapOf<Int, Bitmap>()
+    private final val paint = Paint()
 
+    init {
+        loadBitmaps()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        drawChessboard(canvas)
+        drawPieces(canvas)
+    }
+
+    private fun drawPieces(canvas: Canvas?) {
+
+        //Kings and Queens are unique pieces, so we arrange them by one
+        drawPieceAt(canvas, 3,0, R.drawable.queen_white)
+        drawPieceAt(canvas, 3,7, R.drawable.queen_black)
+        drawPieceAt(canvas, 4,0, R.drawable.king_white)
+        drawPieceAt(canvas, 4,7, R.drawable.king_black)
+
+        // Bishops, Knights and Rooks have come in pairs of 2 so we arrange them by two
+        for (i in 0..1) {
+            drawPieceAt(canvas, 2 + i * 3, 0, R.drawable.bishop_white)
+            drawPieceAt(canvas, 2 + i * 3, 7, R.drawable.bishop_black)
+            drawPieceAt(canvas, 1 + i * 5, 0, R.drawable.king_white)
+            drawPieceAt(canvas, 1 + i * 5, 7, R.drawable.knight_black)
+            drawPieceAt(canvas, 0 + i * 7, 0, R.drawable.rook_white)
+            drawPieceAt(canvas, 0 + i * 7, 7, R.drawable.rook_black)
+        }
+
+        // Pawns come in two groups of 8
+        for (i in 0..7){
+            drawPieceAt(canvas, i,1, R.drawable.pawn_white)
+            drawPieceAt(canvas, i,6, R.drawable.pawn_black)
+        }
+    }
+
+    // Draws a given piece inside a specified cell's borders
+    private fun drawPieceAt(canvas: Canvas?, col: Int, row: Int, resID: Int) {
+        val bitmap = bitmaps[resID]!!
+        canvas?.drawBitmap(bitmap, null, RectF(originX + col * cellSize,originY + (7 - row) * cellSize,originX + (col + 1) * cellSize,originY + ((7 - row) + 1) * cellSize), paint)
+    }
+
+    private fun loadBitmaps() {
+        imgResIDs.forEach {
+            bitmaps[it] = BitmapFactory.decodeResource(resources, it)
+        }
+    }
+
+    // Draws the Chessboard background white and black squares
+    private fun drawChessboard(canvas: Canvas?) {
         val paintWhite = Paint()
-        // (we use Light Gray instead of White because of contrast with the background and the white pieces)
-        paintWhite.color = Color.LTGRAY
+        // (We use the #757575 shade of Gray instead of White because of contrast with the background and the white pieces)
+        paintWhite.color = Color.parseColor("#757575")
 
         val paintBlack = Paint()
-        // (we use Dark Gray instead of Black because of contrast with the black pieces)
-        paintBlack.color = Color.DKGRAY
+        // (We use the #212121 Gray instead of Black because of contrast with the black pieces)
+        paintBlack.color = Color.parseColor("#212121")
 
         /*
          We start counting from square A8 ( (0,7) in the Chessboard Console output string)
          if the sum of the column and the row number is an even number, then we paint the square white.
          If it is an odd number, we paint it black.
          */
-
-
 
         // draw every column
         for (i in 0..7) {
